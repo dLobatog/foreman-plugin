d92f0aa51dc6e45c1168bc6e4c2d247823dd7939
Daniel Lobato Garcia <me@daniellobato.me>
2018-02-16 21:09:03 +0000

    Changes to make it work with REX

--- redhat-access/app/assets/javascripts/insights/app.module.js
+++ redhat-access/app/assets/javascripts/insights/app.module.js
@@ -53,19 +53,29 @@
             InsightsConfigProvider.setGettingStartedLink('https://access.redhat.com/insights/getting-started/satellite/6/');
             InsightsConfigProvider.setAllowExport(true);
             InsightsConfigProvider.setOverviewKey('overview-satellite6');
-            InsightsConfigProvider.setPlannerEnabled(true);    
+            InsightsConfigProvider.setPlannerEnabled(true);
             var initInjector = angular.injector(['ng']);
             var $http = initInjector.get('$http');
-            //TODO fix hard coded values for the ansibleRunner stuff
+            $http.defaults.headers.common = {
+                'X-CSRF-TOKEN': $('meta[name=csrf-token]').attr('content')
+            };
             InsightsConfigProvider.setAnsibleRunner(function ($location, planId, button) {
+                var ansibleRunInputs = {
+                  'organization_id' : REDHAT_ACCESS_SETTINGS.Insights.org_id,
+                  'plan_id' : planId
+                }
+                var ansibleFeatureName = 'ansible_run_insights_plan'
+                var ansibleHostIds = ['juana-sensel.lobatolan.home']
+                // ASK LINDANI HOW TO GET LIST OF HOSTS FOR A PLAN
+
                 if (button === "run") {
                     var data = {
-                        'job_template_id' : 'Ansible: Run Insights maintenance plan',
-                        'inputs' : { 
-                          'organization_id' : REDHAT_ACCESS_SETTINGS.Insights.org_id,
-                          'plan_id' : planId
-                        }
-                      };
+                      'job_invocation' : {
+                        'feature' : ansibleFeatureName,
+                        'host_ids' : ansibleHostIds,
+                        'inputs' : ansibleRunInputs
+                      }
+                    };
                     $http.post('/api/v2/job_invocations/', data)
                     .success(function (response, status, headers) {
                         window.location = '/job_invocations/'+response.id;
@@ -73,10 +83,13 @@
                     .error(function (response, status, header) {
                         alert("failed to create job;");
                     });
-                  
+
                 } else if (button === "customize") {
-                    window.location = "/job_invocations/new?template_id=" + REDHAT_ACCESS_SETTINGS.Insights.AnsibleRunnerTmplId + "&plan_id=" +planId;
-                    //TODO - need to set ansibleRunnerTmplId in app/views/redhat_access/analytics_dashboard/index.html.erb
+                    // NEED TO LOOK INTO HOW TO SEND THIS HASH OF INPUTS
+                    window.location = "/job_invocations/new?feature=" + ansibleFeatureName +
+                      "&host_ids[]=" + ansibleHostIds.join() +
+                      "&inputs[plan_id]=" + ansibleRunInputs['plan_id'] +
+                      "&inputs[organization_id]=" + ansibleRunInputs['organization_id'];
                 }
             });
         }
